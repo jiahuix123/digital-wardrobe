58ab87897324d3e59b5b0c49f8462cf4c3cf2612
lwl0830 <lwl0830@gmail.com>
2014-04-26 18:57:09 -0700

    2

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<title>If you only have 30 seconds!</title>
+		<title>Digital Wardrobe</title>
 		<link href="index.css" type="text/css" rel="stylesheet" />
 	</head>
 	<body>
@@ -19,65 +19,65 @@
 			</div>
 		</header>
 		<!--<div id="whole">-->
-		<div id="signup" class="column">
-			<form action="http://students.washington.edu/jx5/signupredirect.php" method="POST">
-				<fieldset>
-					<legend>Don't have an account yet?</legend>
-					<div class="columnname">First Name:</div>
-						<input type="text" name="firstname" size="25" /> </br>
-					<div class="columnname">Last Name:</div>
-						<input type="text" name="lastname" size="25" /> </br>
+			<div id="signup" class="column">
+				<form action="http://students.washington.edu/jx5/signupredirect.php" method="POST">
+					<fieldset>
+						<legend>Don't have an account yet?</legend>
+						<div class="columnname">First Name:</div>
+							<input type="text" name="firstname" size="25" /> </br>
+						<div class="columnname">Last Name:</div>
+							<input type="text" name="lastname" size="25" /> </br>
 
-					<div class="columnname">Email:</div>
-						<input type="text" name="email" size="25"/> </br>
+						<div class="columnname">Email:</div>
+							<input type="text" name="email" size="25"/> </br>
 
-					<div class="columnname">Password:</div>
-						<input type="password" name="password" size="25" /> </br>
-					<input type="submit" value="Sign me up!" />
-				</fieldset>
-				<?php
-					if (isset($_SESSION["AccountExist"])) {
-						echo $_SESSION["AccountExist"];
-						unset($_SESSION["AccountExist"]);
-					}
-				?>
-			</form>
-		<!--</div>-->
-
-		<!--<div id = "login" class="column">-->
-			<form id="loginform" action="http://students.washington.edu/jx5/loginredirect.php" method="post">
-				
-				<fieldset>
-					<legend>Already have an account!</legend>
-					<div class="columnname">Email:</div>
-					
-					<div>
-						<input id="email" name="email" type="text" size="25"  autofocus="sutofocus">
-					</div>
-					
 						<div class="columnname">Password:</div>
+							<input type="password" name="password" size="25" /> </br>
+						<input type="submit" value="Sign me up!" />
+					</fieldset>
+					<?php
+						if (isset($_SESSION["AccountExist"])) {
+							echo $_SESSION["AccountExist"];
+							unset($_SESSION["AccountExist"]);
+						}
+					?>
+				</form>
+			<!--</div>-->
+
+			<!--<div id = "login" class="column">-->
+				<form id="loginform" action="http://students.washington.edu/jx5/loginredirect.php" method="post">
 					
-					<div>
-						<input id="userpassword" name="password" type="password" size="25" maxlength="20">
-					</div>
+					<fieldset>
+						<legend>Already have an account!</legend>
+						<div class="columnname">Email:</div>
+						
+						<div>
+							<input id="email" name="email" type="text" size="25"  autofocus="sutofocus">
+						</div>
+						
+							<div class="columnname">Password:</div>
+						
+						<div>
+							<input id="userpassword" name="password" type="password" size="25" maxlength="20">
+						</div>
 
-					<div>
-						<input id="submitbutton" type="submit" value="Login" name="login">
-					</div>
-				</fieldset>
-				<?php
-					if (isset($_SESSION["pw_incorrect"])) {
-						 echo $_SESSION["pw_incorrect"]; 
-						 unset($_SESSION["pw_incorrect"]);
-					}
+						<div>
+							<input id="submitbutton" type="submit" value="Login" name="login">
+						</div>
+					</fieldset>
+					<?php
+						if (isset($_SESSION["pw_incorrect"])) {
+							 echo $_SESSION["pw_incorrect"]; 
+							 unset($_SESSION["pw_incorrect"]);
+						}
 
-					if (isset($_SESSION["noAccount"])) {
+						if (isset($_SESSION["noAccount"])) {
 						echo $_SESSION["noAccount"];
-						unset($_SESSION["noAccount"]);
-					}
-				?>
-			</form>
-		</div>
+							unset($_SESSION["noAccount"]);
+						}
+					?>
+				</form>
+			</div>
 		
 	<!--</div>-->
 	</body>
