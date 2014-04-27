7d5cf973f8f67812569db3f03b53f97530b922ce
Jiahui Xu <zjxujiahui@gmail.com>
2014-04-26 19:08:01 -0700

    index fix

--- index.php
+++ index.php
@@ -41,17 +41,6 @@
 			<div id="logo">
 			</div>
 		</header>
-		<!--<div id="whole">-->
-<<<<<<< HEAD
-			<div id="signup" class="column">
-				<form action="http://students.washington.edu/jx5/signupredirect.php" method="POST">
-					<fieldset>
-						<legend>Don't have an account yet?</legend>
-						<div class="columnname">First Name:</div>
-							<input type="text" name="firstname" size="25" /> </br>
-						<div class="columnname">Last Name:</div>
-							<input type="text" name="lastname" size="25" /> </br>
-=======
 		<div id="signup" class="column">
 			<form action="" method="POST">
 				<fieldset>
@@ -60,13 +49,10 @@
 						<input type="text" name="firstname" size="25" /> </br>
 					<div class="columnname">Last Name:</div>
 						<input type="text" name="lastname" size="25" /> </br>
->>>>>>> FETCH_HEAD
 
 						<div class="columnname">Email:</div>
 							<input type="text" name="email" size="25"/> </br>
 
-<<<<<<< HEAD
-=======
 					<div class="columnname">Password:</div>
 						<input type="password" name="password" size="25" /> </br>
 					<input type="submit" value="Sign me up!" />
@@ -78,7 +64,7 @@
 					}
 				?>
 			</form>
-		<!--</div>-->
+	
 
 			<form id="loginform" action="loginredirect.php" method="post">
 				
@@ -89,8 +75,7 @@
 					<div>
 						<input id="email" name="email" type="text" size="25"  autofocus="sutofocus">
 					</div>
-					
->>>>>>> FETCH_HEAD
+
 						<div class="columnname">Password:</div>
 							<input type="password" name="password" size="25" /> </br>
 						<input type="submit" value="Sign me up!" />
@@ -102,43 +87,7 @@
 						}
 					?>
 				</form>
-			<!--</div>-->
-
-			<!--<div id = "login" class="column">-->
-				<form id="loginform" action="http://students.washington.edu/jx5/loginredirect.php" method="post">
-					
-					<fieldset>
-						<legend>Already have an account!</legend>
-						<div class="columnname">Email:</div>
-						
-						<div>
-							<input id="email" name="email" type="text" size="25"  autofocus="sutofocus">
-						</div>
-						
-							<div class="columnname">Password:</div>
-						
-						<div>
-							<input id="userpassword" name="password" type="password" size="25" maxlength="20">
-						</div>
-
-						<div>
-							<input id="submitbutton" type="submit" value="Login" name="login">
-						</div>
-					</fieldset>
-					<?php
-						if (isset($_SESSION["pw_incorrect"])) {
-							 echo $_SESSION["pw_incorrect"]; 
-							 unset($_SESSION["pw_incorrect"]);
-						}
-
-						if (isset($_SESSION["noAccount"])) {
-						echo $_SESSION["noAccount"];
-							unset($_SESSION["noAccount"]);
-						}
-					?>
-				</form>
 			</div>
-		
-	<!--</div>-->
+
 	</body>
 </html>
